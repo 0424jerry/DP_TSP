bcae6b8f0f930e184f57ff33c6432fba8e6c7a42
0424jerry <jerrygithub@gms.ndhu.edu.tw>
2021-06-12 16:35:44 +0800

    Solve Input

--- code/main.cpp
+++ code/main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include<vector>
-
+#include<cmath>
 using namespace std;
 
 
 
 int main() {
+    vector<int> index;
+    vector<pair<float,float>> coordinate;
+    int n ;
+    float x ,y;
+
+   while(cin >> n >> x >> y && n != EOF ){
+
+       index.push_back(n - 1);
+       coordinate.push_back(make_pair(x,y));
+   }
+   int s =  index.size();
+
+    float Map[s][s];
+    for(int i = 0; i < s; i++){
+        for(int j = 0; j < s; j++){
+            Map[i][j] = sqrt( pow(coordinate[i].first - coordinate[j].first , 2)  + pow((coordinate[i].second - coordinate[j].second ) , 2));
+        }
+    }
+
+
+
+
 
     return 0;
 }
